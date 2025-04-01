2544cb03554689eb384a7b7b51a9ad57d88986ec
RupenParthu <rupenparthu@gmail.com>
2025-04-01 08:21:23 +0530

    updated shell sort

--- sorting/shell-sort/code.c
+++ sorting/shell-sort/code.c
@@ -2,8 +2,17 @@
 
 void shellsort(int arr[], int n)
 {
-    for(int gap = n/2; gap > 0; gap/=2){
-        for(int i=gap;i<n;i++)
+    // shell's original sequence
+    for(int gap = n/2;gap>0;gap/=2){
+        for(int i = gap;i<n;i++){
+            int key = arr[i];
+            int j = i;
+            while(j>=gap && arr[j-gap]>key){
+                arr[j] = arr[j-gap];
+                j-=gap;
+            }
+            arr[j] = key;
+        }
     }
 }
 void printarray(int arr[], int n)
